26a27855819c37f52540d546b28145070598908d
Guillaume Guarin <guillaume.guarin@webdrone.fr>
2022-08-22 11:56:38 +0200

    mv-bubblechart

---
+++ files/data.js
@@ -0,0 +1,25 @@
+const data = [{
+    "label": "Websites",
+    "founds" : 3,
+    "value": 20,
+    "image": "./img/website.svg"
+  },{
+    "label": "Emails",
+    "founds" :4,
+    "value": 10,
+    "image": "./img/email.svg"
+  },{
+    "label": "Phone Numbers",
+    "founds": 3,
+    "value": 20,
+    "image": "./img/phone.svg"
+  },{
+    "label": "Social Media",
+    "founds": 5,
+    "value": 5,
+    "image": "./img/phone.svg"
+  }
+  ];
+
+
+  export default data;
