d949bcad7597e6c3aeca3a852fee497d42829779
Guillaume Guarin <guillaume.guarin@webdrone.fr>
2022-09-06 17:49:36 +0200

    randomisation des données au refresh

--- bubblechart/src/js/data.js
+++ bubblechart/src/js/data.js
@@ -1,33 +1,33 @@
 const data = [{
-    "id":1,
-    "label": "Websites",
-    "founds" : 3,
-    "value": 25,
-    "image": "./src/img/website.svg",
-    "link" : "" 
-  },{
-    "id":2,
-    "label": "Emails",
-    "founds" :4,
-    "value": 10,
-    "image": "./src/img/email.svg",
-    "link" : "http://" 
-  },{
-    "id":3,
-    "label": "Phone Numbers",
-    "founds": 3,
-    "value": 30,
-    "image": "./src/img/phone.svg",
-    "link" : "http://" 
-  },{
-    "id":4,
-    "label": "Social Media",
-    "founds": 5,
-    "value": 5,
-    "image": "./src/img/phone.svg",
-    "link" : "http://" 
-  }
-  ];
+  "id": 1,
+  "label": "Websites",
+  "founds": Math.floor(Math.random() * 100),
+  "value": Math.floor(Math.random() * 100),
+  "image": "./src/img/website.svg",
+  "link": ""
+}, {
+  "id": 2,
+  "label": "Emails",
+  "founds": Math.floor(Math.random() * 100),
+  "value": Math.floor(Math.random() * 100),
+  "image": "./src/img/email.svg",
+  "link": "http://"
+}, {
+  "id": 3,
+  "label": "Phone Numbers",
+  "founds": Math.floor(Math.random() * 100),
+  "value": Math.floor(Math.random() * 100),
+  "image": "./src/img/phone.svg",
+  "link": "http://"
+}, {
+  "id": 4,
+  "label": "Social Media",
+  "founds": Math.floor(Math.random() * 100),
+  "value": Math.floor(Math.random() * 100),
+  "image": "./src/img/phone.svg",
+  "link": "http://"
+}
+];
 
 
-  export default data;
+export default data;
